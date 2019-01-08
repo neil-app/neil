dd79dfe58e08455ab9148abe54446092abb3edb2
daichi <daichi.tominaga.1112@gmail.com>
2019-01-08 23:49:42 +0900

    create account

--- presentation/web/nuxt.config.js
+++ presentation/web/nuxt.config.js
@@ -5,9 +5,6 @@ const nuxtEnvs = {
     maintenancePassUserIds: [],
     baseURL: "http://localhost:5000",
     browserBaseURL: "http://localhost:5000",
-    // browserBaseURL: "http://10.0.2.2:5000", // ie
-    intercomAppId: "mdt4c02a",
-    jupyterBaseURL: "http://localhost:8888",
   },
 }
 
@@ -15,40 +12,76 @@ const env = nuxtEnvs[process.env.BUILD_ENV || "local"]
 
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'neil',
+    title: "neil",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ],
+    script: [
+      {
+        src: "//amp.azure.net/libs/amp/latest/azuremediaplayer.min.js"
+      }
     ]
   },
+  css: [{ src: "~assets/main.sass", lang: "sass" }],
+  modules: ["nuxt-fontawesome", "@nuxtjs/axios", "nuxt-buefy"],
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
+    vendor:
+      process.env.NODE_ENV !== "production"
+        ? ["babel-polyfill", "axios", "event-source-polyfill"]
+        : ["babel-polyfill", "axios"],
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
+      }
+      if (isClient) {
+        config.devtool = "source-map";
       }
     }
+  },
+  axios: {
+    // debug: true,
+    requestInterceptor: (config, { store }) => {
+      if (store.state.token) {
+        config.headers.common["Authorization"] = `Bearer ${store.state.token}`;
+      }
+      return config;
+    },
+    baseURL: env.baseURL,
+    browserBaseURL: env.browserBaseURL
+  },
+  fontawesome: {
+    imports: [
+      {
+        set: "@fortawesome/free-solid-svg-icons",
+        icons: ["fas"]
+      }
+    ]
   }
-}
-
+};
