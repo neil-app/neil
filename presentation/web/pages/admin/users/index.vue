dad1bc27092a141addfccf8dc3250b0ebd5221eb
tominagadaichi <daidaidadaidai@softbank.ne.jp>
2019-01-24 16:28:01 +0900

    get users

---
+++ presentation/web/pages/admin/users/index.vue
@@ -0,0 +1,27 @@
+<template>
+
+
+    <div class="columns is-center">
+        <table class="table is-fullwidth">
+        </table>
+    </div>
+
+
+
+</template>
+
+
+
+<script>
+
+  methods: {
+    get() {
+      this.$axios.get(`/api/users/${this.userId}`, {
+         name: this.user.name,
+         email: this.user.email,
+    })
+      this.isEdit = false
+    },
+  }
+
+</script>
