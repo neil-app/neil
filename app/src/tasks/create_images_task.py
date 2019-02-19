ea19630cbc03d32855e8d1d69f47888be3bb3753
hiroyano <ny.ldn.tko@gmail.com>
2019-02-19 22:48:20 +0900

    resolve create image task

--- app/src/tasks/create_images_task.py
+++ app/src/tasks/create_images_task.py
@@ -1,8 +1,8 @@
 import urllib
-from ..database import db
-from ..models import Image
 import requests
 from bs4 import BeautifulSoup
+from ..database import db
+from ..models import Image
 
 
 class CreateImagesTask:
@@ -10,41 +10,41 @@ class CreateImagesTask:
     def __init__(self, url, tag_name):
         self.session = db.session
         self.url = url
-        if tag_name[0] == ".":
-            self.tag_name = tag_name
-        else:
-            self.tag_name = "." + tag_name
+        self.tag_name = self.convert_tag_name(tag_name)
+        self.res = requests.get(url)
+        self.soup = self.scraping(self.res)
 
     def create(self):
-        res = requests.get(self.url)
-        html = res.content
-        soup = BeautifulSoup(html, "html.parser")
-        image_tags = soup.select(self.tag_name)
-        title_tag = soup.title.string
+        title = self.soup.title.string
+        image_tags = self.soup.select(self.tag_name)
         if not image_tags:
             print("{0}が存在しない".format(self.tag_name))
             return
-        imgs = list()
+        for image_tag in image_tags:
+            image_url = self.get_image_url(image_tag)
+            if image_url:
+                self.session.add(Image(title=title, image_url=image_url, user_id=8))
+        self.session.commit()
 
-        for img in image_tags:
-            img = img.find("img") if img.find("img") else img
-            if img.get("src"):
-                src = urllib.parse.unquote(img['src'])
-                imgs.append(src)
+    @staticmethod
+    def get_image_url(image_tag):
+        image_tag = image_tag.find("img") if image_tag.find("img") else image_tag
+        if not image_tag.get("src"):
+            return
+        print(image_tag['src'])
+        image_url = urllib.parse.unquote(image_tag['src'])
+        split_query = urllib.parse.urlparse(image_url).query.split('&')
+        if "src" in split_query:
+            image_url = split_query.split('=')[1]
+        return image_url
 
-        image_urls = list()
-        for img in imgs:
-            for split_query in urllib.parse.urlparse(img).query.split('&'):
-                key = split_query.split('=')[0]
-                value = split_query.split('=')[1]
-                if key == 'src':
-                    image_urls.append(value)
-            print(imgs)
-            print(image_urls)
+    @staticmethod
+    def convert_tag_name(tag_name):
+        if tag_name[0] != ".":
+            return "." + tag_name
+        return tag_name
 
-
-
-        for img_url in image_urls:
-            image = Image(title="title_tag", image_url=img_url, user_id=8)
-            self.session.add(image)
-            self.session.commit()
+    @staticmethod
+    def scraping(res):
+        if res.status_code < 300:
+            return BeautifulSoup(res.content, "html.parser")
