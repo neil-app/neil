9a19c0612741a70c68b387eede9646ed48bd80c8
hiroyano <ny.ldn.tko@gmail.com>
2019-02-20 22:04:32 +0900

    contact

--- presentation/web/pages/index.vue
+++ presentation/web/pages/index.vue
@@ -11,8 +11,7 @@
       <a href="http://www.aoyama-eye-clinic.com/cl_info/">About</a>
       <a href="https://www.shuminoengei.jp/m-pc/a-page_p_detail/target_plant_code-350">Works</a>
       <a href="https://beauty.hotpepper.jp/kr/slnH000286231/blog/W000225989/">Blog</a>
-      <a href="https://yumeg.com/shopdetail/000000001572/">Shop</a>
-      <a href="https://beauty.hotpepper.jp/CSP/kr/reserve/?storeId=H000286231&staffId=W000225989">Contact</a>
+      <nuxt-link :to="{path: `/contact`}">Contact</nuxt-link>
     </div>
     <div class="images">
       <img v-for="image in images" :key="image.id" :src="image.image_url" class="image">
